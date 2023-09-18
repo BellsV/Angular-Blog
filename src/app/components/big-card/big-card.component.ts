4b463001c06d2627ebce780817460a6ee40a484b
Isabella Vargas <isabella2808@gmail.com>
2023-09-18 12:37:55 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string=''
+  @Input()
+  dayleData:string='25 de janeiro de 2024'
+  @Input()
+  cardTitle:string=''
+  @Input()
+  cardDescription:string=''
+  constructor(){}
+
+  ngOnInit(): void {
+  }
 
 }
